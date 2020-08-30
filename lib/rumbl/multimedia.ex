f77273669ed0b958b023c913516eb8c56f8e1d04
Miguel Abaroa <miguelabaroa@Miguels-MacBook-Pro.local>
2020-08-30 01:33:02 -0500

    commit with tests until page 156 with 4 errors of 9 tests

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -1,43 +1,17 @@
 defmodule Rumbl.Multimedia do
-  @moduledoc """
-  The Multimedia context.
-  """
-
   import Ecto.Query, warn: false
   alias Rumbl.Repo
 
   alias Rumbl.Multimedia.Video
   alias Rumbl.Accounts
+  alias Rumbl.Multimedia.Category
 
-  @doc """
-  Returns the list of videos.
-
-  ## Examples
-
-      iex> list_videos()
-      [%Video{}, ...]
-
-  """
   def list_user_videos(%Accounts.User{} = user) do
     Video
     |> user_videos_query(user)
     |> Repo.all()
   end
 
-  @doc """
-  Gets a single video.
-
-  Raises `Ecto.NoResultsError` if the Video does not exist.
-
-  ## Examples
-
-      iex> get_video!(123)
-      %Video{}
-
-      iex> get_video!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_user_video!(%Accounts.User{} = user, id) do
     Video
     |> user_videos_query(user)
@@ -48,18 +22,6 @@ defmodule Rumbl.Multimedia do
     from(v in query, where: v.user_id == ^user_id)
   end
 
-  @doc """
-  Creates a video.
-
-  ## Examples
-
-      iex> create_video(%{field: value})
-      {:ok, %Video{}}
-
-      iex> create_video(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
@@ -67,50 +29,27 @@ defmodule Rumbl.Multimedia do
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a video.
-
-  ## Examples
-
-      iex> update_video(video, %{field: new_value})
-      {:ok, %Video{}}
-
-      iex> update_video(video, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_video(%Video{} = video, attrs) do
     video
     |> Video.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a video.
-
-  ## Examples
-
-      iex> delete_video(video)
-      {:ok, %Video{}}
-
-      iex> delete_video(video)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_video(%Video{} = video) do
     Repo.delete(video)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking video changes.
-
-  ## Examples
-
-      iex> change_video(video)
-      %Ecto.Changeset{source: %Video{}}
-
-  """
   def change_video(%Video{} = video) do
     Video.changeset(video, %{})
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
   end
 end
--- lib/rumbl_web/controllers/video_controller.ex
+++ lib/rumbl_web/controllers/video_controller.ex
@@ -4,6 +4,8 @@ defmodule RumblWeb.VideoController do
   alias Rumbl.Multimedia
   alias Rumbl.Multimedia.Video
 
+  plug :load_categories when action in [:new, :create, :edit, :update]
+
   def index(conn, _params, current_user) do
     videos = Multimedia.list_user_videos(current_user)
     render(conn, "index.html", videos: videos)
@@ -15,7 +17,7 @@ defmodule RumblWeb.VideoController do
   end
 
   def create(conn, %{"video" => video_params}, current_user) do
-    case Multimedia.create_video(conn.assigns.current_user, video_params) do
+    case Multimedia.create_video(current_user, video_params) do
       {:ok, video} ->
         conn
         |> put_flash(:info, "Video created successfully.")
@@ -63,5 +65,9 @@ defmodule RumblWeb.VideoController do
   def action(conn, _) do
     args = [conn, conn.params, conn.assigns.current_user]
     apply(__MODULE__, action_name(conn), args)
+  end
+
+  defp load_categories(conn, _) do
+    assign(conn, :categories, Multimedia.list_alphabetical_categories())
   end
 end
